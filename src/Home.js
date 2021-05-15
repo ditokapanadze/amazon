b8369ac1146459f702ebfabe3b0f6c7cceef0a28
ditokapanadze <ditokapanadze111@gmail.com>
2021-05-15 07:17:52 -0700

    context api added

--- src/Home.js
+++ src/Home.js
@@ -10,15 +10,53 @@ function Home() {
           src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg"
         />
         <div className="home__row">
-          <Product />
-          {/* {product} */}
+          <Product
+            id
+            title="the lean startup: How Constant innovation Creates Radically Successfull business"
+            price={29.99}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+          />
+          <Product
+            id="49538094"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kittchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+          />
         </div>
         <div className="home__row">
-          {/* {product} */}
-          {/* {product} */}
-          {/* {product} */}
+          <Product
+            id="4903850"
+            title="Samsung L345 49' Curved"
+            price={199.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            id="23445930"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={199.99}
+            rating={3}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="325435435"
+            title="New Apple iPod Pro (12.9-inch, Wi-Fi, 128GB) = Silver (4th generation)"
+            price={598.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          />
         </div>
-        <div className="home__row">{/* {product} */}</div>
+        <div className="home__row">
+          <Product
+            id="908299322"
+            title="Samsung LC$9RG90SSUXEN 59' Curved"
+            price={1098.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+          />
+        </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Product.css";
-function Product() {
+import { useStateValue } from "./StateProvider";
+
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  console.log(basket);
+  const addTobasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        iamge: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
-          <smal>$</smal> <strong>898</strong>
+          <smal>$</smal> <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>⭐</p>
+          {/* არაის ვქმნით რატინგისგან რო იმდენჯერ დავარენდეროთ რა რიცხვსაც გადმოვცემთ */}
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
-      <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" />
-      <button>Add to Basket</button>
+      <img src={image} />
+      <button onClick={addTobasket}>Add to Basket</button>
     </div>
   );
 }
